b3ec05a03aca75143a37deb1a8a3e1ef1cc7445c
Ludovica735 <ludovicacappelluccio7@gmail.com>
2026-09-07 10:36:12 +0200

    Aggiunta pagina elenco allievi e aggiornato menu di navigazione

--- ScuolaDanza/gestione/urls.py
+++ ScuolaDanza/gestione/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('corsi/', views.elenco_corsi, name='elenco_corsi'),
     path('corsi/<int:corso_id>/', views.dettaglio_corso, name='dettaglio_corso'),
+    path('allievi/', views.elenco_allievi, name='elenco_allievi'),
 ]
--- ScuolaDanza/gestione/views.py
+++ ScuolaDanza/gestione/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from .models import Corso, Iscrizione
+from .models import Corso, Iscrizione, Allievo
 
 # Create your views here.
 def home(request):
@@ -14,3 +14,7 @@ def dettaglio_corso(request, corso_id):
     corso = Corso.objects.get(id_corso=corso_id)
     iscrizioni = Iscrizione.objects.filter(corso=corso)  #prendi tutte le iscrizioni che appartengono a questo corso
     return render(request, 'gestione/dettaglio_corso.html', {'corso': corso, 'iscrizioni': iscrizioni})
+
+def elenco_allievi(request):
+    allievi = Allievo.objects.all()
+    return render(request, 'gestione/allievi.html', {'allievi': allievi})
