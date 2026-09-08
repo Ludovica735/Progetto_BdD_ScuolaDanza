f126c7ea729771bd34c3fc5700b5843457da26f0
Ludovica735 <ludovicacappelluccio7@gmail.com>
2026-09-08 12:04:17 +0200

    Aggiunta Login e dettagli delle altre sezioni

--- ScuolaDanza/gestione/urls.py
+++ ScuolaDanza/gestione/urls.py
@@ -7,4 +7,14 @@ urlpatterns = [
     path('corsi/<int:corso_id>/', views.dettaglio_corso, name='dettaglio_corso'),
     path('allievi/', views.elenco_allievi, name='elenco_allievi'),
     path('team/', views.team, name='team'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('area-allievo/', views.area_allievo, name='area_allievo'),
+    path('area-insegnante/', views.area_insegnante, name='area_insegnante'),
+    path('area-amministratore/', views.area_amministratore, name='area_amministratore'),
+    path('area-genitore/', views.area_genitore, name='area_genitore'),
+    path('orari/', views.orari, name='orari'),
+    path('saggi/', views.elenco_saggi, name='elenco_saggi'),
+    path('saggi/<int:saggio_id>/', views.dettaglio_saggio, name='dettaglio_saggio'),
 ]
+
--- ScuolaDanza/gestione/views.py
+++ ScuolaDanza/gestione/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
-from .models import Corso, Iscrizione, Allievo, Insegnante, Amministratore, HaFiglio
+from .models import Utente, Corso, Iscrizione, Allievo, Insegnante, Amministratore, Genitore, HaFiglio, Lezione, Saggio, PartecipaSaggio
+
+from django.shortcuts import redirect
 
 # Create your views here.
 def home(request):
@@ -11,13 +13,25 @@ def elenco_corsi(request):
     return render(request, 'gestione/corsi.html', {'corsi': corsi})
 
 def dettaglio_corso(request, corso_id):
+    if request.session.get('ruolo') not in ('amministratore', 'insegnante'):
+        return redirect('login')
     corso = Corso.objects.get(id_corso=corso_id)
-    iscrizioni = Iscrizione.objects.filter(corso=corso)  #prendi tutte le iscrizioni che appartengono a questo corso
-    return render(request, 'gestione/dettaglio_corso.html', {'corso': corso, 'iscrizioni': iscrizioni})
+    iscrizioni = Iscrizione.objects.filter(corso=corso)
+    ruolo = request.session.get('ruolo')
+    return render(request, 'gestione/dettaglio_corso.html', {'corso': corso, 'iscrizioni': iscrizioni, 'ruolo': ruolo})
 
 def elenco_allievi(request):
+    if request.session.get('ruolo') != 'amministratore':
+        return redirect('login')
     allievi = Allievo.objects.all()
-    return render(request, 'gestione/allievi.html', {'allievi': allievi})
+    lista_allievi = []
+    for allievo in allievi:
+        genitore = None
+        ha_figlio = HaFiglio.objects.filter(allievo=allievo).first()
+        if ha_figlio:
+            genitore = ha_figlio.genitore
+        lista_allievi.append({'allievo': allievo, 'genitore': genitore})
+    return render(request, 'gestione/allievi.html', {'lista_allievi': lista_allievi})
 
 def team(request):
     insegnanti = Insegnante.objects.all()
@@ -34,3 +48,85 @@ def elenco_allievi(request):
             genitore = ha_figlio.genitore
         lista_allievi.append({'allievo': allievo, 'genitore': genitore})
     return render(request, 'gestione/allievi.html', {'lista_allievi': lista_allievi})
+
+def login_view(request):
+    errore = None
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        try:
+            utente = Utente.objects.get(email=email, password=password)
+            request.session['id_utente'] = utente.id_utente
+            request.session['nome_utente'] = f"{utente.nome} {utente.cognome}"
+
+            if Allievo.objects.filter(utente=utente).exists():
+                request.session['ruolo'] = 'allievo'
+                return redirect('area_allievo')
+            elif Insegnante.objects.filter(utente=utente).exists():
+                request.session['ruolo'] = 'insegnante'
+                return redirect('area_insegnante')
+            elif Amministratore.objects.filter(utente=utente).exists():
+                request.session['ruolo'] = 'amministratore'
+                return redirect('area_amministratore')
+            elif Genitore.objects.filter(utente=utente).exists():
+                request.session['ruolo'] = 'genitore'
+                return redirect('area_genitore')
+        except Utente.DoesNotExist:
+            errore = "Email o password non corretti."
+    return render(request, 'gestione/login.html', {'errore': errore})
+
+
+def logout_view(request):
+    request.session.flush()
+    return redirect('home')
+
+def area_allievo(request):
+    if request.session.get('ruolo') != 'allievo':
+        return redirect('login')
+    allievo = Allievo.objects.get(utente__id_utente=request.session['id_utente'])
+    iscrizioni = Iscrizione.objects.filter(allievo=allievo)
+    partecipazioni_saggi = PartecipaSaggio.objects.filter(allievo=allievo)
+    return render(request, 'gestione/area_allievo.html', {'allievo': allievo, 'iscrizioni': iscrizioni})
+
+
+def area_insegnante(request):
+    if request.session.get('ruolo') != 'insegnante':
+        return redirect('login')
+    insegnante = Insegnante.objects.get(utente__id_utente=request.session['id_utente'])
+    corsi = Corso.objects.filter(insegnante=insegnante)
+    return render(request, 'gestione/area_insegnante.html', {'insegnante': insegnante, 'corsi': corsi})
+
+
+def area_amministratore(request):
+    if request.session.get('ruolo') != 'amministratore':
+        return redirect('login')
+    amministratore = Amministratore.objects.get(utente__id_utente=request.session['id_utente'])
+    corsi = Corso.objects.all()
+    saggi = Saggio.objects.filter(amministratore=amministratore)
+    return render(request, 'gestione/area_amministratore.html', {'amministratore': amministratore, 'corsi': corsi, 'saggi': saggi})
+
+def area_genitore(request):
+    if request.session.get('ruolo') != 'genitore':
+        return redirect('login')
+    genitore = Genitore.objects.get(utente__id_utente=request.session['id_utente'])
+    figli = HaFiglio.objects.filter(genitore=genitore)
+    figli_con_corsi = []
+    for figlio in figli:
+        iscrizioni = Iscrizione.objects.filter(allievo=figlio.allievo)
+        figli_con_corsi.append({'allievo': figlio.allievo, 'iscrizioni': iscrizioni})
+    return render(request, 'gestione/area_genitore.html', {'genitore': genitore, 'figli_con_corsi': figli_con_corsi})
+
+def orari(request):
+    lezioni = Lezione.objects.all().order_by('data', 'ora_inizio')
+    return render(request, 'gestione/orari.html', {'lezioni': lezioni})
+
+def elenco_saggi(request):
+    saggi = Saggio.objects.all().order_by('data')
+    return render(request, 'gestione/saggi.html', {'saggi': saggi})
+
+def dettaglio_saggio(request, saggio_id):
+    if request.session.get('ruolo') != 'amministratore':
+        return redirect('login')
+    saggio = Saggio.objects.get(id_saggio=saggio_id)
+    partecipazioni = PartecipaSaggio.objects.filter(saggio=saggio)
+    return render(request, 'gestione/dettaglio_saggio.html', {'saggio': saggio, 'partecipazioni': partecipazioni})
